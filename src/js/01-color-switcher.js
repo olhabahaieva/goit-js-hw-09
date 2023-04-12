3915f4cc7939b5ab4851b4c1d18770946b24dd19
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-12 21:20:28 +0300

    1st task: added disabled inline style when. start or stop button is enabled

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,10 +1,30 @@
 //Start button
-const startButton = document.querySelector('button[data-start="true"]');
+const startButton = document.querySelector('button[data-start]');
 
 // Stop button
-const stopButton = document.querySelector('button[data-stop="true"]');
+const stopButton = document.querySelector('button[data-stop]');
 
 //Function - Random color generator
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
   }
+
+//Event listener for start button
+startButton.addEventListener('click', onClickStart);
+
+//Event listener for stop button
+stopButton.addEventListener('click', onClickStop);
+
+//Function onClick for startButton
+function onClickStart(evt){
+    evt.preventDefault();
+    startButton.disabled = true;
+    stopButton.disabled = false;
+}
+
+//Function onClick for stopButton
+function onClickStop(evt){
+    evt.preventDefault();
+    stopButton.disabled = true;
+    startButton.disabled = false;
+}
