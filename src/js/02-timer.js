57ad5be034698f43693c4b530fae731e0203dfb6
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-12 23:00:00 +0300

    2nd js: added flatpickr to my input

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,3 +2,8 @@
 import flatpickr from "flatpickr";
 // Дополнительный импорт стилей
 import "flatpickr/dist/flatpickr.min.css";
+
+//Input to select a date
+const inputDate = document.querySelector('#datetime-picker');
+
+flatpickr(inputDate);
