fed444a16ac9fc2af4eca74b8798bbca50c8838b
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-12 23:06:52 +0300

    2nd task: added additional options for the flatpiker

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,4 +6,16 @@ import "flatpickr/dist/flatpickr.min.css";
 //Input to select a date
 const inputDate = document.querySelector('#datetime-picker');
 
-flatpickr(inputDate);
+//Options for the date and time for flatpikr
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+
+flatpickr(inputDate, options);
+
