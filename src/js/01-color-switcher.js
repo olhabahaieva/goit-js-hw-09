e0ba31e3d7246a56e6e5d7a7356a451ae4619814
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-12 20:57:05 +0300

    1 task - added selectors and random color function

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,10 @@
+//Start button
+const startButton = document.querySelector('button[data-start="true"]');
 
+// Stop button
+const stopButton = document.querySelector('button[data-stop="true"]');
+
+//Function - Random color generator
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
