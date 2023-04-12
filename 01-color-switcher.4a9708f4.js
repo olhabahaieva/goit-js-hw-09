232affb679597519f0aff5e32da503e46319e966
olhabahaieva <bahaieva.olha@gmail.com>
2023-04-12 18:52:16 +0000

    Deploying to gh-pages from @ olhabahaieva/goit-js-hw-09@1e33d050d0eb16925e5de49d022bcc29bbde3a27 🚀

---
+++ 01-color-switcher.4a9708f4.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body");let n=null;t.addEventListener("click",(function(d){d.preventDefault(),t.disabled=!0,e.disabled=!1,n=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(a){a.preventDefault(),e.disabled=!0,t.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.4a9708f4.js.map
