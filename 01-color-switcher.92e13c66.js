f636ad69024287d65ab4800d78e746a6f226a498
olhabahaieva <bahaieva.olha@gmail.com>
2023-04-12 17:57:28 +0000

    Deploying to gh-pages from @ olhabahaieva/goit-js-hw-09@e0ba31e3d7246a56e6e5d7a7356a451ae4619814 🚀

---
+++ 01-color-switcher.92e13c66.js
@@ -0,0 +1,2 @@
+document.querySelector('button[data-start="true"]'),document.querySelector('button[data-stop="true"]');
+//# sourceMappingURL=01-color-switcher.92e13c66.js.map
---
+++ 01-color-switcher.eaa7c6c3.js
@@ -0,0 +1,2 @@
+document.querySelector('button[data-start="true"]'),document.querySelector('button[data-stop="true"]');
+//# sourceMappingURL=01-color-switcher.eaa7c6c3.js.map
