2bf8a31fbdee61278c0e2c8b16172494bbf720ae
olhabahaieva <bahaieva.olha@gmail.com>
2023-04-12 18:47:40 +0000

    Deploying to gh-pages from @ olhabahaieva/goit-js-hw-09@f0263dac60590a332e1e3401b6b029d8d4eec58b 🚀

--- 01-color-switcher.4f0fe8d9.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]");e.addEventListener("click",(function(d){d.preventDefault(),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(d){d.preventDefault(),t.disabled=!0,e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.4f0fe8d9.js.map
--- 01-color-switcher.52e8a83d.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]");e.addEventListener("click",(function(d){d.preventDefault(),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(d){d.preventDefault(),t.disabled=!0,e.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.52e8a83d.js.map
