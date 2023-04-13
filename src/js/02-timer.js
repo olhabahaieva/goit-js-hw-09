fc59f5ad1ff528b306875a71b16dd23c2d1ad9bb
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-13 16:50:45 +0300

    2nd js - added timer functionality

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,60 +6,75 @@ import "flatpickr/dist/flatpickr.min.css";
 //Input to select a date
 const inputDate = document.querySelector('#datetime-picker');
 
+//Days digits selector
+const daysDigits = document.querySelector('.value[data-days');
+
+//Hours digits selector
+const hoursDigits = document.querySelector('.value[data-hours]');
+
+//Minutes digits selector
+const minutesDigits = document.querySelector('.value[data-minutes]');
+
+//Seconds digits selector
+const secondsDigits = document.querySelector('.value[data-seconds]');
+
 //Start button
 const startButton = document.querySelector('.button-start');
 startButton.disabled = true;
 
-//Start button event listener
-startButton.addEventListener('click', convertMs());
-
-//Date today
-const today = new Date();
+//// Declare selectedDate variable outside of functions
+let selectedDate;
 
 //Options for the date and time for flatpikr
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        const selectedDate = selectedDates[0];
-        if(selectedDate < today){
-            startButton.disabled = true;
-            window.alert("Please choose a date in the future");
-          } else{
-            console.log(selectedDates[0]);
-            startButton.disabled = false;
-          }
-      
-      
-    },
-  };
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedDate = selectedDates[0];
+    if (selectedDate < new Date()) {
+      startButton.disabled = true;
+      window.alert("Please choose a date in the future");
+    } else {
+      console.log(selectedDate);
+      startButton.disabled = false;
+    }
+  },
+};
 
 flatpickr(inputDate, options);
 
 
 //function for timer
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  setInterval(() => {
+    const diffMs = selectedDate - new Date();
+    daysDigits.textContent = Math.floor(diffMs / day);
+    hoursDigits.textContent = Math.floor((diffMs % day) / hour);
+    minutesDigits.textContent = Math.floor(((diffMs % day) % hour) / minute);
+    secondsDigits.textContent = Math.floor((((diffMs % day) % hour) % minute) / second);
+  }, 1000);
+
+  return { days, hours, minutes, seconds };
+}
   
   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
+//Start button event listener
+startButton.addEventListener('click', () => {
+  const diffMs = selectedDate - new Date();
+  convertMs(diffMs);
+})
