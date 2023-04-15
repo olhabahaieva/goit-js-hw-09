a38bc64c058ee474993af4a16ea831583e7fc29a
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-15 14:59:10 +0300

    added basic elements and code to the 3rd task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,24 @@
+//First delay input
+const inputFirstDelay = document.querySelector('input[name=delay]');
+
+//Second delay input
+const inputDelayStep = document.querySelector('input[name=step]');
+
+//Amount input
+const inputAmount = document.querySelector('input[name=amount]');
+
+//Button element
+const buttonElement = document.querySelector('button[type=submit]');
+
+//Event listener
+buttonElement.addEventListener("click", onSubmit);
+
+//Function for the button click
+function onSubmit(evt){
+   evt.preventDefault();
+   createPromise();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +27,11 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
