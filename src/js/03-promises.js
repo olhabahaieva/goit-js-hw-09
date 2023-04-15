80a8fe9c9007d1cff4e09472f3af13f5aefc3d33
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-15 21:01:12 +0300

    corrected notiflix library

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,32 +4,35 @@ import Notiflix from 'notiflix';
 const form = document.querySelector('.form');
 
 //Event listener
-form.addEventListener("submit", onFormSubmit);
+form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
-  event.preventDefault()
-  const {delay, step,amount } = event.target.elements;
-  for(let i = amount; i< amount; i+=1){
-    createPromise(position, delay).then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
+  event.preventDefault();
+  const { delay, step, amount } = event.target.elements;
+  for (let i = amount; i < amount; i += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
-  }
+}
 
 function createPromise(position, delay) {
   return new Promise((res, rej) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve > 0.3) {
-        res({position, delay});
+        res({ position, delay });
+        Notiflix.Notify.success('Sol lucet omnibus');
       } else {
-        rej({position, delay});
+        rej({ position, delay });
+        Notiflix.Notify.failure('Qui timide rogat docet negare');
       }
     }, delay);
-  })
+  });
 }
 
 //Testing promise
