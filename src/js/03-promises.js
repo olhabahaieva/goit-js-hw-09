6492af7aa7cf17ebdc29db18df4bae496cc6b804
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-15 20:58:16 +0300

    fully new solution for 3rd task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,58 +1,34 @@
 import Notiflix from 'notiflix';
 
 //Form element
-const form = document.querySelectorAll('.form');
-
-//First delay input
-const inputFirstDelay = document.querySelector('input[name=delay]');
-
-//Second delay input
-const inputDelayStep = document.querySelector('input[name=step]');
-
-//Amount input
-const inputAmount = document.querySelector('input[name=amount]');
-
-//Button element
-const buttonElement = document.querySelector('button[type=submit]');
+const form = document.querySelector('.form');
 
 //Event listener
-buttonElement.addEventListener("click", onStart);
+form.addEventListener("submit", onFormSubmit);
 
-function onStart() {
-  const promises = [...form].map((item, idx) => {
-    item.value = "";
-    const promise = createPromise(idx);
-    promise
-      .then((value) => (item.textContent = value))
-      .catch((err) => (item.textContent = err));
-    return promise;
-  });
-
-  Promise.allSettled(promises).then((promise) => {
-    const isAllRej = promise.every(({ status }) => status === "rejected");
-    const isAllRes = promise.every(({ status }) => status === "fulfilled");
-
-    setTimeout(() => {
-      if (isAllRej || isAllRes) {
-        Notiflix.Notify.failure('Qui timide rogat docet negare');
-      } else {
-        Notiflix.Notify.success('Sol lucet omnibus');
-      }
-
-    }, 1000);
-  })
-}
+function onFormSubmit(event) {
+  event.preventDefault()
+  const {delay, step,amount } = event.target.elements;
+  for(let i = amount; i< amount; i+=1){
+    createPromise(position, delay).then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  }
+  }
 
 function createPromise(position, delay) {
   return new Promise((res, rej) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve > 0.3) {
-        res( Notiflix.Notify.success('Sol lucet omnibus'));
+        res({position, delay});
       } else {
-        rej(Notiflix.Notify.failure('Qui timide rogat docet negare'));
+        rej({position, delay});
       }
-    }, 0);
+    }, delay);
   })
 }
 
