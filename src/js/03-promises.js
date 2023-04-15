77fbd71eb0abb947f0ba67a9000975960fa3759f
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-15 16:43:21 +0300

    added first promise code base

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,6 @@
+//Form element
+const form = document.querySelectorAll('.form');
+
 //First delay input
 const inputFirstDelay = document.querySelector('input[name=delay]');
 
@@ -11,21 +14,47 @@ const inputAmount = document.querySelector('input[name=amount]');
 const buttonElement = document.querySelector('button[type=submit]');
 
 //Event listener
-buttonElement.addEventListener("click", onSubmit);
+buttonElement.addEventListener("click", onStart);
 
-//Function for the button click
-function onSubmit(evt){
-   evt.preventDefault();
-   createPromise();
+function onStart(){
+ const promises = [...form].map((item, idx)=>{
+  item.textContent = "";
+
+  const promise = createPromise(idx);
+  promise
+  .then((value) => (item.textContent = value))
+  .catch((err) => (item.textContent = err));
+  return promise;
+})
 }
 
+Promise.allSettled(promises).then((promise)=>{
+  const isAllRej = promise.every(({ status }) => status === "rejected");
+  const isAllRes = promise.every(({ status }) => status === "fulfilled");
+
+  setTimeout(()=>{
+    if(isAllRej){
+      alert("Rejected");
+    } else {
+      alert("Success");
+    }
+    
+  }, inputAmount);
+
+})
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((res, rej)=>{
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(()=> {
+      if (shouldResolve > 0.3) {
+        res(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        rej(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+   
+  }, delay * 1000);
+})
 }
 
 createPromise(2, 1500)
