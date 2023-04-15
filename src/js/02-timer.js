c50e25d8ae856186b58d52415791e63a26e0ac40
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-16 00:41:57 +0300

    corrected code for 2nd task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -50,42 +50,45 @@ flatpickr(inputDate, options);
 
 //function for timer
 function convertMs(ms) {
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
+  // Remaining days
   const days = Math.floor(ms / day);
+  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-  if (!startButton.disabled) {
-    const diffMs = selectedDate - new Date();
+  return { days, hours, minutes, seconds };
+}
 
-    daysDigits.textContent = addLeadingZero(Math.floor(diffMs / day));
-    hoursDigits.textContent = addLeadingZero(Math.floor((diffMs % day) / hour));
-    minutesDigits.textContent = addLeadingZero(
-      Math.floor(((diffMs % day) % hour) / minute)
-    );
-    secondsDigits.textContent = addLeadingZero(
-      Math.floor((((diffMs % day) % hour) % minute) / second)
-    );
-  } else {
-    return { days, hours, minutes, seconds };
-  }
+
+//Timer digits update
+function updateTimerDisplay({ days, hours, minutes, seconds }) {
+  daysDigits.textContent = addLeadingZero(days);
+  hoursDigits.textContent = addLeadingZero(hours);
+  minutesDigits.textContent = addLeadingZero(minutes);
+  secondsDigits.textContent = addLeadingZero(seconds);
 }
 
 //Start button event listener
 startButton.addEventListener('click', () => {
-  const diffMs = selectedDate - new Date();
+  let diffMs = selectedDate - new Date();
   if (diffMs > 0) {
     intervalID = setInterval(() => {
-      convertMs(diffMs);
-      addLeadingZero(diffMs);
+      const timeObj = convertMs(diffMs);
+      updateTimerDisplay(timeObj);
+      diffMs -= 1000;
     }, 1000);
   }
 });
+
 
 function addLeadingZero(value) {
   return value.toString().padStart(2, '0');
