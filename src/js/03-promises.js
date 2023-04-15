ce7f7ba9a1570c91c3c680dbb28913e1e6328f71
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-16 00:45:10 +0300

    3rd task - removed createPromise example

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -38,12 +38,3 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
-
-//Testing promise
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
